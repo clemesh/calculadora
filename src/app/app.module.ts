ceeacb8a86930e5a822f96fc98857f2342f82499
Cleme <cgc351969@gmail.com>
2021-11-09 22:49:54 -0600

    Inicio calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperadorComponent } from './operador/operador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperadorComponent
   ],
   imports: [
     BrowserModule,
